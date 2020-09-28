59e6f0279b3cce4d89bea50dda778e3562c542f7
yasanomer <yasanomer@gmail.com>
2020-09-27 21:35:25 -0400

    class work

--- src/test/java/com/cybertek/utilities/SmartBearUtilities.java
+++ src/test/java/com/cybertek/utilities/SmartBearUtilities.java
@@ -28,20 +28,75 @@ public class SmartBearUtilities {
         WebElement loginButton = driver.findElement(By.id("ctl00_MainContent_login_button"));
         loginButton.click();
     }
-
+/*
     public static void verifyOrder (WebDriver driver, String name ){
     List<WebElement> allNames=driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//td[2]"));
 
-    boolean listOfNames = false;
+   // boolean listOfNames = false;  (solution 1)
+
 
     for (WebElement eachName:allNames){
         if(eachName.getText().equals(name)){
-           listOfNames = true;
-           break;
+       //    listOfNames = true;  (solution 1)
+            Assert.assertTrue(true);
+          // break;
+            return;
         }
     }
-        Assert.assertTrue(listOfNames);
+      //  Assert.assertTrue(listOfNames); (solution 1)
+        Assert.fail("Name does not in the list!");
 
     }
 
+
+ */
+
+    public static void verifyOrder(WebDriver driver, String name){
+
+        //Created a locator that only returns the names of the costumer from all of the rows
+        //Created a list and stored the cells that has costumer name information into it.
+        List<WebElement> namesList = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr/td[2]"));
+
+        //If there is only one table on the page, you dont have to locate the table. --> //tr/td[2]
+        //List<WebElement> namesList = driver.findElements(By.xpath("//tr/td[2]"));
+
+
+        //type iter, press tab for "for each" loop short cut.
+
+        for (WebElement each : namesList) {
+
+            if (each.getText().equals(name)){
+                Assert.assertTrue(each.getText().equals(name));
+                return;
+            }
+        }
+        // Assert fail method will fail the test when it is called, no matter what.
+        Assert.fail("Name does not exist in the list! Verification FAILED!!!");
+
+    }
+
+    /*
+  • Create a method named printNamesAndCities in SmartBearUtils class.
+  • Method takes WebDriver object.
+  • This method should simply print all the names in the List of All Orders.
+  • Create a new TestNG test to test if the method is working as expected.
+  • Output should be like:
+  • Name1: name , City1: city
+  • Name2: name , City2: city
+   */
+    public static void printNamesAndCities(WebDriver driver){
+
+        //locating all the names from all the rows and storing in a list of web element
+        List<WebElement> namesList = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr/td[2]"));
+
+        //we need to locate all the cities from all the rows
+        List<WebElement> citiesList = driver.findElements(By.xpath("//table[@id='ctl00_MainContent_orderGrid']//tr/td[7]"));
+
+        for (int i = 0; i < namesList.size() ; i++) {
+
+            System.out.println("Name"+ (i+1) +": " + namesList.get(i).getText() + ", City"+ (i+1) +": " + citiesList.get(i).getText());
+
+        }
+
+    }
 }
