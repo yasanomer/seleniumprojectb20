300a1b669b83fac591c727474dd50632b7edc004
yasanomer <yasanomer@gmail.com>
2020-10-14 13:08:23 -0400

    update

---
+++ src/test/java/com/cybertek/pages/LoginPage.java
@@ -0,0 +1,15 @@
+package com.cybertek.pages;
+
+import com.cybertek.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public LoginPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(id = "demo")
+    public WebElement usernameInput;
+
+}
---
+++ src/test/java/com/cybertek/tests/Library/jira6_US1_AC5.java
@@ -0,0 +1,5 @@
+package com.cybertek.tests.Library;
+
+public class jira6_US1_AC5 {
+
+}
